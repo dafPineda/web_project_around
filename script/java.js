8375d9f87d05ad55e7455c2de3ef3a7b0807d925
Dafne Pineda <152250662+dafPineda@users.noreply.github.com>
2025-10-20 05:43:17 -0600

    forms

---
+++ script/java.js
@@ -0,0 +1,47 @@
+let buttonEdit = document.querySelector('.profile__button-edit');
+let editBlock = document.querySelector('.form');
+let buttonClose = document.querySelector('.form__info-close');
+let buttonSave = document.querySelector('.form__info__button');
+let inputName = document.querySelector('#name');
+let inputOcupacion = document.querySelector('#work'); 
+let userName = document.querySelector('.profile__name');
+let userWork = document.querySelector('.profile__ocupation');
+
+buttonSave.disabled = true;
+
+
+function editProfile() {
+  editBlock.style.display = 'block';
+}
+
+function closeProfile() {
+  editBlock.style.display = 'none';
+}
+
+function validarCampos() {
+    let nameFull = inputName.value.trim();
+    let ocupationFull = inputOcupacion.value.trim();
+    if( nameFull !== "" && ocupationFull !== ""){
+        buttonSave.disabled = false;
+    }else{
+        buttonSave.disabled = true;
+    }
+}
+function saveProfile(){
+    userName.textContent = inputName.value.trim();
+    userWork.textContent = inputOcupacion.value.trim();
+
+    inputName.value = "";
+    inputOcupacion.value = "";
+    buttonSave.disabled = true;
+    closeProfile();
+}
+
+buttonEdit.addEventListener('click', editProfile);
+buttonClose.addEventListener('click', closeProfile);
+
+inputName.addEventListener('input', validarCampos);
+inputOcupacion.addEventListener('input', validarCampos);
+
+buttonSave.addEventListener('click', saveProfile);
+
